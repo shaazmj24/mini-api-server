d01833d6533824dee960aa730240477becca6d0d
smeg24 <meghanishaaz@gmail.com>
2026-07-14 16:30:34 +0500

    Stage 0: hello server

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI 
-import random 
 
 app = FastAPI() 
 
 @app.get("/") 
-def hello_world(): 
-    return {"message": "hello world"} 
-
-@app.get("/random")   
-def random_num():   
-    num = random.randint(1, 11)
-    return {"Random number" : num}
+def root():   
+    return {"message" : "hello world"}
 
